7cc82a1fed206ffa717972773b42cbb38ade00d2
Dwi Wahyu <madecahya72@gmail.com>
2021-02-06 17:33:46 +0800

    add swiper slide to review

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,35 @@ window.onscroll = function () {
     navbar.classList.remove("sticky");
     navbar.classList.add("bg-custom");
   }
-};
+}
+
+// Lightcase
+jQuery(document).ready(function ($) {
+  $('a[data-rel^=lightcase]').lightcase();
+});
+
+// Swiper
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    }
+  }
+});
